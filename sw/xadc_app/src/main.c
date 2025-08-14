a41a3f15d6e062cc4f3bcf3448445c416b54d8ac
lukaexmed <kalsekluka@gmail.com>
2025-08-15 03:27:21 +0800

    hehe

--- sw/xadc_app/src/main.c
+++ sw/xadc_app/src/main.c
@@ -27,15 +27,29 @@ int main(){
 
     	if(counter >= limit){
     		//light mechanism
-    		if(lights == 0xFFFF || lights == 0x0000)
-    			rising = !rising;
-    		lights = rising ? (lights << 1) + 1 : (lights >> 1) ;//rising lights
-    		write_output(lights);
+    		//if(lights == 0xFFFF || lights == 0x0000)
+    		//	rising = !rising;
+    		//lights = rising ? (lights << 1) + 1 : (lights >> 1) ;//rising lights
+    		//write_output(lights);
+
+    		uint16_t raw = (uint16_t)read_adc(0) & 0x0FFF;
+
+    		    	    // 2) compute how many LEDs: round(raw*16/4095)
+    		    	    uint8_t leds_on = (raw * 16 + 2047) / 4095;
+
+    		    	    // 3) build a 16-bit mask: lowest leds_on bits = 1
+    		    	    uint16_t mask = (leds_on == 0)
+    		    	    				? 0
+    		    	    				: (uint16_t)((1U << leds_on) - 1);
+
+    		    	    // 4) output to the LEDs
+    		    	    write_output(mask);
+
 
     		seconds++;
-    		uint32_t temp = (uint32_t)read_adc(0);
-    		//double temp = 1234.7890;
-    		uint32_t display = 0;
+    		uint16_t temp = (uint16_t)read_adc(5);
+    		//uint16_t temp = 1234.7890;
+    		uint16_t display = 0;
     		//converting from hex to dec
     		for(int i = 0; i < 8; i++){
     			display = display | ((temp % 10) << i*4); //
--- sw/xadc_app/src/xadc.c
+++ sw/xadc_app/src/xadc.c
@@ -7,16 +7,17 @@
 #define read(offset)	(*(volatile uint32_t *)(BRIDGE_BASE + (((SLOT) << 5) + offset) * 4))
 
 
-double read_adc(int n){
+uint16_t read_adc(int n){
 
 	//0-3 are adc inputs, 4-5 are internals
 	uint32_t data = (uint16_t) read(n) & 0x0000ffff; //mask to fit 16bit status register
-	data = (data >> 4) /4096; // adc is 12 bit, hence the shift is needed, /4096
+	//data = (data >> 4) /4096;
+	data = (data >> 4);// adc is 12 bit, hence the shift is needed, /4096
 	if(n == VCC_REG){
-		data *= 3.0;
+		data *= 300000.0;
 	}
 	else if(n == TMP_REG){
 		data = data * 503.975 - 273.15;
 	}
-	return ((double)data);
+	return data;
 }
