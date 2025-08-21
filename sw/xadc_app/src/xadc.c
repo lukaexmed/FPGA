d130c369dc606ab7b350bb99be8345b8a0301ecd
LazarNedelja <lazalaki069@gmail.com>
2025-08-21 20:59:34 +0200

    SW working

--- sw/xadc_app/src/xadc.c
+++ sw/xadc_app/src/xadc.c
@@ -8,19 +8,10 @@
 
 
 uint32_t read_adc(int n){
-uint32_t read_adc(int n){
 
 	//0-3 are adc inputs, 4-5 are internals
 	uint32_t data = read(n) & 0x0000ffff; //mask to fit 16bit status register
 	//data = (data >> 4) /4096;
 	data = (data >> 4);// adc is 12 bit, hence the shift is needed, /4096
-	/*if(n == VCC_REG){
-		//data je med 0x000 in 0xfff to je med 0.0V in 1.0V
-		data *= 24;
-		data /= 10;
-	}
-	else if(n == TMP_REG){
-		data = data * 503.975 - 273.15;
-	}*/
 	return data;
 }
